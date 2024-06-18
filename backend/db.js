8481eb6cf95cc97d21391b5b0c7566acc060e1dd
Mohd. Irfan Lohar <irfan007lohar@gmail.com>
2024-06-18 22:58:16 +0530

    Updated db.js

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const connectToMongo = async () => {
     try {
       mongoose.set("strictQuery", false);
-      // await mongoose.connect("mongodb+srv://irfan:eJrFVt4EDQ1hmj5q@cluster0.jszufzb.mongodb.net/test?retryWrites=true&w=majority");
-      await mongoose.connect("mongodb://127.0.0.1:27017/movie_maestro")
+      await mongoose.connect("mongodb+srv://irfan:eJrFVt4EDQ1hmj5q@cluster0.jszufzb.mongodb.net/test?retryWrites=true&w=majority");
+      // await mongoose.connect("mongodb://127.0.0.1:27017/movie_maestro")
       console.log("Connected to Mongo Successfully!");
     } catch (error) {
       console.log(error);
